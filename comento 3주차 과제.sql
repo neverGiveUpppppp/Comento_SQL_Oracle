f55be46cad948ea17e16ec520700c688fd1efb03
Robert Choi <95724704+neverGiveUpppppp@users.noreply.github.com>
2022-10-03 17:53:11 +0900

    solution added

--- comento 3주차 과제.sql
+++ comento 3주차 과제.sql
@@ -88,6 +88,22 @@ where retire_ymd = '99991231'
 order by retire_ymd desc;
 
 
+-- SELF JOIN : FAM_C + FAM_C
+SELECT F1.EMP_NO, F2.FAM_NM
+FROM FAM_C F1
+    JOIN FAM_C F2 ON(F1.EMP_NO = F2.FAM_NM) 
+-- EMP_NO이 숫자고 FAM_NM이 STR이라 데이터타입 오류 발생
+;
+SELECT F1.EMP_NO, F2.FAM_NM
+FROM FAM_C F1
+    JOIN FAM_C F2 ON(F1.EMP_NO = F2.EMP_NO) 
+;    
+  
+
+
+select * from FAM_C; 
+
+
 
 --3)20210321 기준으로 재직중인 임직원이며,사내부부인 임직원의 직원번호를 구하는 sql을 작성하세요.
 --HINT : EMP_C와 FAM_REL_C를 조인해야 합니다.
@@ -107,7 +123,8 @@ WHERE E.EMP_NO = FR.EMP_REL_NO  -- 직원번호와 직원가족번호 =조건 해봤으나 X
 SELECT *
 FROM EMP_C E
     JOIN FAM_REL_C FR ON(E.EMP_NO = FR.EMP_REL_NO)
-WHERE E.EMP_NO = FR.EMP_REL_NO  -- 직원번호와 직원가족번호 =조건 해봤으나 X
+    -- JOIN ON절의 컬럼을 바꿔서 해봄
+WHERE E.EMP_NO = FR.EMP_REL_NO  
 ;    
 
 SELECT *
@@ -140,7 +157,7 @@ WHERE EMP_NO = 11501824;
 -- 재직중 : RETIRE 99/12/31 조건
 -- 현재 날짜 기준
 -- 이씨성 : LIKE '이%'
--- 자녀수
+-- 자녀수 : COUNT
 
 -- 조건 : 재직중+이씨성
 SELECT *
@@ -223,3 +240,132 @@ FROM
 GROUP BY
     org_nm;    
 ;    
+
+
+
+
+-----------------------------------------------------------------
+------------------------- 3주차 과제 모범답안 ----------------------
+-----------------------------------------------------------------
+
+
+--1)생년월일이?20120101?에서?20151231?사이인 쌍둥이 자녀를 가진 임직원을 구하세요.
+--HINT1: FAM_C테이블과?FAM_C테이블을 서로?조인해야합니다.?조인 시 쌍둥이의 조건에 대해 잘 생각해보세요.?이름이 다르고,?생년월일이 같음
+
+SELECT
+  FAM1.EMP_NO AS 직원번호
+, FAM1.FAM_NM AS 쌍둥이1
+, FAM2.FAM_NM AS 쌍둥이2
+FROM FAM_C FAM1
+	INNER JOIN FAM_C FAM2
+		ON FAM1.EMP_NO = FAM2.EMP_NO
+AND FAM1.FAM_NM <> FAM2.FAM_NM
+AND FAM1.REL_TYPE_CD = FAM2.REL_TYPE_CD
+AND FAM1.REL_TYPE_CD = 'A27'
+AND FAM1.BIRTH_YMD = FAM2.BIRTH_YMD
+;
+
+
+--2) 20210321?기준으로 재직중인 임직원이며,?생년월일이?20120101?에서?20151231인 자녀를 가진 임직원을 구하는?sql을 작성하세요.
+--HINT : EMP_C와?FAM_C를 조인해야 합니다.
+
+SELECT
+EMP.EMP_NO AS 직원번호
+,FAM.FAM_NM AS 자녀성명
+,FAM.BIRTH_YMD AS 자녀생년월일
+ FROM EMP_C EMP
+LEFT OUTER JOIN FAM_C FAM
+ON EMP.EMP_NO = FAM.EMP_NO
+AND FAM.REL_TYPE_CD = 'A27'
+WHERE '20210321' BETWEEN EMP.HIRE_YMD AND RETIRE_YMD
+AND FAM.BIRTH_YMD BETWEEN '20120101' AND '20151231'
+ORDER BY FAM.BIRTH_YMD
+;
+
+
+--3) 20210321?기준으로 재직중인 임직원이며, 사내부부인 임직원의 직원번호를 구하는?sql을 작성하세요.
+--HINT : EMP_C와?FAM_REL_C를 조인해야 합니다.
+
+SELECT
+EMP.EMP_NO AS 직원번호
+,FAM.FAM_NM AS 자녀성명
+,FAM.BIRTH_YMD AS 자녀생년월일
+ FROM EMP_C EMP
+LEFT OUTER JOIN FAM_C FAM
+ON EMP.EMP_NO = FAM.EMP_NO
+AND FAM.REL_TYPE_CD = 'A27'
+WHERE '20210321' BETWEEN EMP.HIRE_YMD AND RETIRE_YMD
+AND FAM.BIRTH_YMD BETWEEN '20120101' AND '20151231'
+ORDER BY FAM.BIRTH_YMD
+;
+
+
+
+--3-2) 현재 날짜 기준 재직중인?“이씨 성＂을 가진 직원의?자녀명수가?몇 명인지 리스트를 만드세요.
+--힌트:?substr?, count(*)를 사용
+
+SELECT
+EMP_NM
+,COUNT(*)
+FROM EMP_C EMP
+INNER JOIN FAM_C FAM
+ON EMP.EMP_NO = FAM.EMP_NO
+AND FAM.REL_TYPE_CD = 'A27'
+AND SYSDATE BETWEEN EMP.HIRE_YMD AND EMP.RETIRE_YMD
+WHERE -- 또는 emp.emp_nm like '이%'
+SUBSTR(EMP_NM,1,1) = '이'
+GROUP BY EMP_NM
+;
+
+
+
+--3-3) 자녀 명수가?2명 이상인 직원의 리스트를 구하세요.??(직원번호?/?성명?/?자녀명수?)
+--힌트?having
+
+SELECT
+ABC.EMP_NO
+,EMP.EMP_NM
+,ABC.CNT
+FROM
+EMP_C EMP
+INNER JOIN
+    (SELECT
+    EMP_NO,
+    COUNT(*) AS CNT
+    FROM FAM_C
+    WHERE REL_TYPE_CD= 'A27'
+    GROUP BY EMP_NO
+    HAVING COUNT(*) >= 2
+    ) ABC
+ON EMP.EMP_NO = ABC.EMP_NO
+;
+
+
+
+
+
+--3-4) 현재 기준 조직의 부서별 직원이 몇 명인지 구하는?sql을 작성하세요. (조직코드,?조직명,?직원수)
+--힌트?group by?org_cd,?org_nm
+
+SELECT
+ORG.ORG_CD
+,ORG.ORG_NM
+,COUNT(*)
+FROM
+ EMP_C EMP
+ INNER JOIN ORG_C ORG
+ ON EMP.ORG_CD = ORG.ORG_C
+ AND SYSDATE BETWEEN ORG.STA_YMD AND ORG.END_YMD
+ AND SYSDATE BETWEEN EMP.HIRE_YMD AND EMP.RETIRE_YMD
+GROUP BY ORG.ORG_CD, ORG.ORG_NM
+;
+
+
+
+
+
+
+
+
+
+
